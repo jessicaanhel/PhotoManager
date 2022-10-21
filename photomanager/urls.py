729062c9cda504a909d41fe710e0cc9367f7ab1b
jessicaanhel <angellas.xo@gmail.com>
2022-10-21 16:40:46 +0200

    create main page and named space  for photo list

--- photomanager/urls.py
+++ photomanager/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from viewer.views import test_view
+from viewer.models import Album, Photo
+from viewer.views import IndexView
+
+admin.site.register(Album)
+admin.site.register(Photo)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test', test_view)
+    path('', IndexView.as_view(), name='index'),
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -1,4 +1,49 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 
-def test_view(request):
-    return HttpResponse('Hello')
+from django.urls import reverse_lazy
+
+from viewer.models import Photo
+from viewer.forms import PhotoForm
+
+
+def photos(request):
+    s1 = request.GET.get('s1, ''')
+    return render(
+        request, template_name='photos.html',
+        context={'photos': Photo.objects.all()})
+
+
+
+class PhotoListView(ListView):
+    template_name = 'photo_list.html'
+    model = Photo
+
+
+class PhotoDetailView(DetailView):
+    template_name = 'photo_detail.html'
+    model = Photo
+
+
+class PhotoCreateView(CreateView):
+    template_name = 'form.html'
+    form_class = PhotoForm
+    success_url = reverse_lazy('viewer:photo_list')
+
+
+class PhotoUpdateView(UpdateView):
+    template_name = 'form.html'
+    model = Photo
+    form_class = PhotoForm
+    success_url = reverse_lazy('viewer:photo_list')
+
+
+class PhotoDeleteView(DeleteView):
+    template_name = 'photo_confirm_delete.html'
+    model = Photo
+    success_url = reverse_lazy('viewer:photo_list')
+
+class IndexView(PhotoListView):
+    template_name = 'index.html'
