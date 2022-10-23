2b480f124ee2828d8a68538707f3b27d7ada96ed
jessicaanhel <angellas.xo@gmail.com>
2022-10-23 22:53:03 +0200

    correct eslint

--- photomanager/urls.py
+++ photomanager/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from viewer.models import Album, Photo
 from viewer.views import IndexView
 
-admin.site.register(Album)
 admin.site.register(Photo)
+admin.site.register(Album)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
