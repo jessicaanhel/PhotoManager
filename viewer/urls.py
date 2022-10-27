97e269276e81504233c67b5a44862f97746fa44d
jessicaanhel <angellas.xo@gmail.com>
2022-10-27 02:08:15 +0200

    change update path to delete

--- viewer/urls.py
+++ viewer/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('photo/detail/<pk>', PhotoDetailView.as_view(), name='photo_detail'),
     path('photo/delete', PhotoListView.as_view(), name='photo_confirm_delete'),
     path('photo/update/<pk>', PhotoUpdateView.as_view(), name='photo_update'),
-    path('photo/delete/<pk>', PhotoUpdateView.as_view(), name='photo_delete'),
+    path('photo/delete/<pk>', PhotoDeleteView.as_view(), name='photo_delete'),
     path('photo/create', PhotoCreateView.as_view(), name='photo_create'),
     ]
